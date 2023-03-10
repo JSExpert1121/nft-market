232e109eada3ba46d9633ab7eeb95c6c83757f51
JSExpert1121 <jingang0314@gmail.com>
2023-03-10 15:38:08 -0300

    feat: 404 page

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,5 +1,5 @@
 export const Footer = () => (
   <footer className='flex justify-center items-center h-12 text-gray-700 bg-gray-50'>
-    Test project for altura by &nbsp; <i>YuanZhe Cui</i>
+    Test project for altura by&nbsp;<i>YuanZhe Cui</i>
   </footer>
-)
+);
